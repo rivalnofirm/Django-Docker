dbf8ad7a7f79a0ed2cd3f5c42b4e7289e6009818
rivalnofirm <rivalnofirm>
2021-09-08 01:18:22 +0700

    update admin custome user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,7 +17,6 @@ class AdminSiteTests(TestCase):
             password = 'test123',
             name = 'test user full name'
         )
-
     
     def test_users_listed(self):
         """Test that users are listed on user page"""
@@ -27,14 +26,12 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-
     def test_user_page_change(self):
         """Test that the user edit page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-
 
     def test_create_user_page(self):
         """Test that the create user page works"""
